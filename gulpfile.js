5d8352714ee342b837635b74f6ae57e95fd8278b
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-05-08 13:31:02 +0100

    Add documentation to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,27 @@ var path = require('path');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 
+/*
+  This is the task runner used to compile the assets. It does the following:
+   - compiles and concatenates less files
+   - copies images
+   - copies fonts
+   - watches for changes and recompiles
+
+
+  Some things it will do:
+   - clean build directory before copying compiled assets
+   - compress images
+   - compile js (not sure whether will use coffeescript yet)
+   - compile jinja templates down to byte code
+   - catch stream errors properly
+
+  We don't need to worry about inlining images or anything like that as Google
+  App Engine supports the SPDY protocol extension which removes almost all of
+  the overhead associated with multiple requests.
+
+*/
+
 var paths = {
   css: ['./assets/css/**/*.less'],
   images: ['./assets/images/**/*'],
